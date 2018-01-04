707a668863d752d5484407cd47a30fae9a39eecb
Shannenpio <Shannenpio@users.noreply.github.com>
2018-01-05 02:30:57 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: 'RyEn3QLb61U', start: 3 };
+	var options = { videoId: 'cUqkYiP_rcE', start: 3 };
 	$('#wrapper').tubular(options);
 	// yLeQAS5xSAg sakitnya tuh disiniiii
 	// 1opp3AiTGdU Goyang dumang mannngggggg
